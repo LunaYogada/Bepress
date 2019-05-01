e842cd84df9b1bea2d1a646fbf76ef7e97e297ac
kaki <liujiaqi2013hit@gmail.com>
2019-05-01 14:16:56 -0700

    generate for loop

--- non linear regression on downloads:uploads.R
+++ non linear regression on downloads:uploads.R
@@ -1,3 +1,7 @@
+library(broom)
+library(ggplot2)
+library(tidyverse)
+
 sample <- content_repo%>%
   filter(site_key == 3006)%>%
 #  mutate(time = (lead(years)-2003)*12+ lead(months) - 4)%>%
@@ -15,7 +19,56 @@ y = sample$downloads/sd(sample$downloads)
 model1=nls(y~SSgompertz(x,Asym,b2,b3))
 summary(model1)
 
-plot(x,y)
+plot(x,y, xlab = "standardized total uploads", ylab = "standardized downloads")
 lines(x, predict(model1), col = "red")
+
+ggplot()+
+  geom_point(aes(x = x, y= y))+
+  geom_line(aes(x = x, y= predict(model1)), col = "red")
+
+
+result_df <- data.frame(matrix(numeric(),ncol = 4, nrow = nrow(sites)))
+colnames(result_df) <- c("site_key","parameter","summary","residual SE")
+
+for(i in 1:nrow(sites)){
+  key = sites[[i, "site_key"]]
+  if(!key %in% c(2,5,8, 1794,2730)){
+    tryCatch({
+      print(paste0("now is modeling for", key,"-----------"))
+      data<-content_repo%>%
+        filter(site_key == key)
+      x = data$cum_uploads
+      y = data$downloads
+      models = nls(y~SSgompertz(x,Asym,b2,b3))
+      
+      #calculate aic aicc bic
+      #  nn=ncol(t(zsales))								# sample size
+      #  pp=ncol(t(est))								# number of parameters
+      # residuals
+      #  sig2=sum(err^2) /(nn-pp)						# sigma^2 of error term
+      # tvals for inference: abs(tval) > 1. 65 => 90% confidence level; abs(tval) > 1.96 => 95% CI
+      #  aic = nn*log(sig2) + 2*pp						# Use when nn is large (i.e., pp/nn < 5%)
+      # bic = nn*log(sig2) + pp*log(nn)					# Use when nn is large (i.e., pp/nn < 5%)
+
+
+      # store results into dataframe
+      result_df[[i, "site_key"]] <- key
+      result_df[[i, "parameter"]] <-tidy(models)
+      result_df[[i, "summary"]] <-summary(models)
+      result_df[[i, "residual SE"]] <-summary(models)$sigma
+      
+      # save plot of model prediction
+      plt <- ggplot()+
+        geom_point(aes(x = x, y= y))+
+        geom_line(aes(x = x, y= predict(models)), col = "red")+
+        labs(x = "standardized total uploads", y = "standardized downloads", title ="gompertz model prediction")+
+        theme_bw()
+      ggsave(paste0( "plots/nonlinear/",key, "model.jpg"), plot = plt)},
+      
+      error=function(e){
+        print(paste0(key,"ERROR :",conditionMessage(e)))
+        })
+  }else{ print(paste0("not modeling for", key))}
+}
 
 
